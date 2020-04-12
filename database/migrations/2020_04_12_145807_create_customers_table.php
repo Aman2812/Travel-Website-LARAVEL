fee22df9b3753175e79418eaacc6b017bcc9ded1
Aman2812 <49606294+Aman2812@users.noreply.github.com>
2020-04-12 21:54:29 +0530

    Add files via upload

---
+++ database/migrations/2020_04_12_145807_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('First_Name');
+            $table->string('Last_Name');
+            $table->string('Email');
+            $table->string('Password');
+            $table->string('Phone_Number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
